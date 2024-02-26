8006478c231e1f8be3a2933eba01801549b60e22
Alyssa <129678823+alyssaHee@users.noreply.github.com>
2024-02-26 15:45:44 -0600

    Add files via upload

---
+++ bmpWriteToSDcard.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_BMP280.h>
+//#include <RTClib.h>
+
+
+File myFile;
+
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+  ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(18)) {
+  Serial.println("initialization failed!");
+  while (1);
+  }
+
+  
+  
+  Serial.println(" SD IS CHILL :3");
+ 
+  unsigned status;
+    
+    //bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+    //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+    status = bmp.begin();
+    
+    if (!status) {
+      Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                         "try a different address!"));
+      while (1) 
+      delay(10);
+    }
+    
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+     
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+
+  Serial.println("initialization done. :3");
+}
+
+
+
+void loop() {
+  sensors_event_t temp_event, pressure_event;
+
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+
+  if(SD.exists("CANSAT_DATA.txt")){
+    SD.remove("CANSAT_DATA.txt");
+  }
+  myFile = SD.open("CANSAT_DATA.txt", FILE_WRITE);
+
+  if (myFile) {
+    myFile.print("Temp: ");
+    myFile.print(temp_event.temperature);
+    myFile.print("\t");
+    myFile.print("Pres: ");
+    myFile.println(pressure_event.pressure);
+  
+    Serial.print(temp_event.temperature);
+    Serial.print(",");
+    Serial.println(pressure_event.pressure);
+  
+   myFile.close();
+
+   delay(2000);
+  } 
+  else {
+  // if the file didn't open, print an error:
+  Serial.println("error opening CANSAT_DATA.txt");
+  }
+
+}
